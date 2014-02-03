c57e2c1f32eab647cf331c2420e25011cec9f5e6
Ian Springer <ispringer@evergage.com>
2014-02-03 08:58:08 -0500

    refactor to be a single directive, rather than two

--- spectrum-angular.js
+++ spectrum-angular.js
@@ -8,146 +8,129 @@
 
 angular.module('spectrum', [])
 
-  .factory('$spectrumHelper', function($window, $log) {
-    function initOptions(scope, element, attrs, ngModel, defaultOptions) {
-      var options = angular.extend(defaultOptions, scope.$eval(attrs.options));
+.directive('spectrum', function($log, $window, $timeout) {
+  var DEFAULT_OPTIONS = {
+    preferredFormat: 'hex6',
+    showInput: true,
+    showAlpha: true,
+    showButtons: false,
+    clickoutFiresChange: true
+  };
 
-      if (!options.change) {
-        options.change = function(tinyColor) {
-          updateModelValue(scope, ngModel, tinyColor);
-        }
-      }
+  function link(scope, element, attrs, ngModel) {
+    var input = element.find('input');
 
-      if (!options.move && !options.showButtons) {
-        options.move = function(tinyColor) {
-          updateModelValue(scope, ngModel, tinyColor);
-        };
-      }
-
-      return options;
+    if (!isSpectrumAvailable(input)) {
+      logError('spectrum not found - make sure spectrum.js is loaded.');
+      return;
     }
 
-    function updateModelValue(scope, controller, tinyColor) {
-      scope.$apply(function() {
-        var color = tinyColor.toString();
-        $log.info('Setting $scope.color to [' + color + ']...');
-        controller.$setViewValue(color);
-      });
-    }
+    var options = initOptions(scope, attrs, ngModel);
 
-    function updateViewValue(controller, element) {
-      var color = controller.$viewValue || '';
-      $log.info('Setting spectrum color to [' + color + ']...');
-      if (isSpectrumInitialized(element)) {
-        element.spectrum('set', color);
-      }
-    }
-
-    function isSpectrumInitialized(element) {
-       return (element.spectrum('container').filter('.sp-container').length == 1);
-    }
-
-    return {
-      initOptions: initOptions,
-      
-      updateViewValue: updateViewValue,
-      
-      isSpectrumInitialized: isSpectrumInitialized,
-      
-      logError: ($window.console.error || $window.console.log)
-    };
-  })
-
-  .directive('spectrum', function($spectrumHelper, $log) {
-    function link(scope, element, attrs, ngModel) {
-      var defaultOptions = {
-        clickoutFiresChange: true,
-        preferredFormat: 'hex6',
-        showButtons: false,
-        showAlpha: true,
-      };
-      
-      var options = 
-        $spectrumHelper.initOptions(scope, element, attrs, ngModel, defaultOptions);
-     
-      if (options.flat) {
-        $spectrumHelper.logError('Use spectrum-flat directive, rather than spectrum directive.');
-        return;
-      }
-      
-      $log.info('Initializing spectrum with options:', options);
-      element.spectrum(options);
-      
-      scope.$watch('ngModel', function(newVal) {
-        $spectrumHelper.updateViewValue(ngModel, element);
-      });
-    }
-
-    return {
-      restrict: 'A',
-      scope: {
-        ngModel: '=',
-        options: '@'
-      },
-      require: '?ngModel',
-      link: link
-    };
-  })
-
-  .directive('spectrumFlat', function($spectrumHelper, $log, $timeout) {
-    function link(scope, element, attrs, ngModel) {
-      var span = element.find('span');
-      
-      var defaultOptions = {
-        flat: true,
-        showInput: true,
-        clickoutFiresChange: true,
-        preferredFormat: 'hex6',
-        showButtons: false,
-        showAlpha: true
-      };
-      
-      var options = 
-        $spectrumHelper.initOptions(scope, span, attrs, ngModel, defaultOptions);
-
-      if (!options.flat) {
-        $spectrumHelper.logError('Use spectrum directive, rather than spectrum-flat directive.');
-        return;
-      }
-
+    var initialized = false;
+    if (options.flat) {
       // When the 'flat' option is true, spectrum cannot render itself correctly unless its parent element is visible,
-      // so in that case, hold off on initializing spectrum until the first time its parent becomes visible.        
+      // so in that case, hold off on initializing spectrum until the first time its parent becomes visible.
       scope.init = function() {
-        var initialized = $spectrumHelper.isSpectrumInitialized(span);
-        if (!initialized && element.parent().is(':visible')) {
-          initialized = true;
-          $timeout(function() {                        
-            options.color = ngModel.$viewValue || '';
-            $log.info('Initializing spectrum with options:', options);
-            span.spectrum(options);
-            setTimeout(function() {
-              span.spectrum('reflow');
-            }, 2500);
-          });
+        initialized = initFlatSpectrumIfParentVisible(element, ngModel, options);
+        if (initialized) {
+          scope.init = function() {
+            return true;
+          };
         }
         return initialized;
-      };
-      
-      scope.$watch('ngModel', function(newVal) {
-        $spectrumHelper.updateViewValue(ngModel, span);
-      });
+      }
+    } else {
+      initialized = true;
+      initSpectrum(ngModel, input, options);
+      scope.init = function() {
+        return true;
+      }
     }
 
-    return {
-      restrict: 'E',
-      require: '?ngModel',
-      scope: {
-        ngModel: '=',
-        options: '@'
-      },
-      replace: true,
-      template: '<div ng-show="init()"><span></span></div>',
-      link: link
-    };
+    scope.$watch('ngModel', function(newValue, oldValue) {
+      if (initialized) {
+        updateViewValue(ngModel, input);
+      }
+    });
+  }
 
-  });
+  function isSpectrumAvailable(element) {
+    try {
+      var spectrumContainer = element.spectrum('container');
+      return (spectrumContainer.length == 1);
+    } catch (e) {
+      return false;
+    }
+  }
+
+  function logError() {
+    return ($window.console.error || $window.console.log);
+  }
+
+  function initOptions(scope, attrs, ngModel) {
+    var options = angular.copy(DEFAULT_OPTIONS);
+    angular.extend(options, scope.$eval(attrs.options));
+
+    if (!options.change) {
+      options.change = function(tinyColor) {
+        updateModelValue(scope, ngModel, tinyColor);
+      }
+    }
+
+    if (!options.move && !options.showButtons) {
+      options.move = function(tinyColor) {
+        updateModelValue(scope, ngModel, tinyColor);
+      };
+    }
+
+    return options;
+  }
+
+  function updateModelValue(scope, controller, tinyColor) {
+    scope.$apply(function() {
+      var color = tinyColor.toString();
+      $log.info('Setting $scope.color to [' + color + ']...');
+      controller.$setViewValue(color);
+    });
+  }
+
+  function updateViewValue(controller, element) {
+    var color = controller.$viewValue || '';
+    $log.info('Setting spectrum color to [' + color + ']...');
+    element.spectrum('set', color);
+  }
+
+  function initFlatSpectrumIfParentVisible(element, ngModel, options) {
+    if (element.parent().is(':visible')) {
+      $timeout(function() {
+        var input = element.find('input');
+        initSpectrum(ngModel, input, options);
+        setTimeout(function() {
+          input.spectrum('reflow');
+        }, 2000);
+      });
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function initSpectrum(ngModel, input, options) {
+    options.color = ngModel.$viewValue || '';
+    console.log('Initializing spectrum with options:', options);
+    input.spectrum(options);
+  }
+
+  return {
+    restrict: 'EA',
+    scope: {
+      ngModel: '=',
+      options: '@'
+    },
+    require: '?ngModel',
+    replace: true,
+    template: '<span ng-show="init()"><input></span>',
+    link: link
+  };
+});
